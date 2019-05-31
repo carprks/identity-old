be3cce3efbc0a4e611030fb17e7dec9b90114f91
Max Hooton <max.hooton@auden.co.uk>
2019-05-31 11:40:39 +0100

    bad but might work

--- .travis-deploy/docker.sh
+++ .travis-deploy/docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    pip install --upgrade pip
-    pip install --upgrade python
+    sudo  pip install --upgrade pip
+    sudo pip install --upgrade python
     pip install --user awscli
     export PATH=$PATH:$HOME/.local/bin
     eval $(aws ecr get-login --no-include-email --region $AWS_DB_REGION)
