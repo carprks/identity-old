e8b59bb74f7ec5038aec0dc440a450eaa43a5029
Max Hooton <max.hooton@auden.co.uk>
2019-05-31 11:14:54 +0100

    hmm

--- .travis-deploy/deploy.sh
+++ .travis-deploy/deploy.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     bash .travis-deploy/ecs.sh -c $CLUSTER -n $APP -i "$AWS_ECR/$APP:latest" -r $AWS_DB_REGION -t 240
+
+    if [ "$TRAVIS_BRANCH" == "master" ]; then
+        CLUSTER=$LIVE_CLUSTER
+        bash .travis-deploy/ecs.sh -c $CLUSTER -n $APP -i "$AWS_ECR/$APP:latest" -r $AWS_DB_REGION -t 240
+    fi
 fi
