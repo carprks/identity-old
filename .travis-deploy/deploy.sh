58e5322a6aee2f6badfef34c67367dc928bb19bb
Max Hooton <max.hooton@auden.co.uk>
2019-05-31 10:38:27 +0100

    see if this deploys to live aswell as dev

--- .travis-deploy/deploy.sh
+++ .travis-deploy/deploy.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env bash
-if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    if [ "$TRAVIS_BRANCH" == "master" ]; then
-        bash .travis-deploy/ecs.sh -c $CLUSTER -n $APP -i "$AWS_ECR/$APP:latest" -r $AWS_DB_REGION -t 240
-    fi
-fi
+bash .travis-deploy/ecs.sh -c $CLUSTER -n $APP -i "$AWS_ECR/$APP:latest" -r $AWS_DB_REGION -t 240
