d4e09bdc3781ad7504ecb315af96d150af91da40
Max Hooton <max.hooton@auden.co.uk>
2019-05-31 10:47:07 +0100

    dont want pull requests to deploy

--- .travis-deploy/deploy.sh
+++ .travis-deploy/deploy.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-bash .travis-deploy/ecs.sh -c $CLUSTER -n $APP -i "$AWS_ECR/$APP:latest" -r $AWS_DB_REGION -t 240
+if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    bash .travis-deploy/ecs.sh -c $CLUSTER -n $APP -i "$AWS_ECR/$APP:latest" -r $AWS_DB_REGION -t 240
+fi
--- .travis-deploy/docker.sh
+++ .travis-deploy/docker.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-pip install --upgrade pip
-pip install --upgrade python
-pip install --user awscli
-export PATH=$PATH:$HOME/.local/bin
-eval $(aws ecr get-login --no-include-email --region $AWS_DB_REGION)
-docker tag "$APP:latest" "$AWS_ECR/$APP:$TRAVIS_COMMIT"
-docker tag "$APP:latest" "$AWS_ECR/$APP:latest"
-docker push "$AWS_ECR/$APP:$TRAVIS_COMMIT"
-docker push "$AWS_ECR/$APP:latest"
+if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    pip install --upgrade pip
+    pip install --upgrade python
+    pip install --user awscli
+    export PATH=$PATH:$HOME/.local/bin
+    eval $(aws ecr get-login --no-include-email --region $AWS_DB_REGION)
+    docker tag "$APP:latest" "$AWS_ECR/$APP:$TRAVIS_COMMIT"
+    docker tag "$APP:latest" "$AWS_ECR/$APP:latest"
+    docker push "$AWS_ECR/$APP:$TRAVIS_COMMIT"
+    docker push "$AWS_ECR/$APP:latest"
+fi
